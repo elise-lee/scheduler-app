4f5d436218a6a406e6ff7bc2e6b70e081a42f083
Elise Lee <eliselee2021@u.northwestern.edu>
2021-04-23 16:07:51 -0500

    course detail screen

--- components/CourseSelector.js
+++ components/CourseSelector.js
@@ -3,7 +3,7 @@ import { View, StyleSheet } from 'react-native';
 import { hasConflict } from '../utils/course'
 import Course from './Course';
 
-const CourseSelector = ({courses}) => {
+const CourseSelector = ({courses, view}) => {
     const [selected, setSelected] = useState([]);
   
     const toggle = course => setSelected(selected => (
@@ -18,6 +18,7 @@ const CourseSelector = ({courses}) => {
               isDisabled={hasConflict(course, selected)}
               isSelected={selected.includes(course) ? true : false}
               select={toggle}
+              view={view}
             />
           ))
         }
